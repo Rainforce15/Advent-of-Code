a86d96a0c45a54dada05d4d9bfbd7f95a953f410
rainforce15 <rainforce15@gmail.com>
2023-01-06 22:04:49 +0100

    improved 2015/1/A and B by one byte each

--- tasks/2015/01/B_space.js
+++ tasks/2015/01/B_space.js
@@ -2,4 +2,4 @@
 module.exports=n=>{let f=0,i=0;for(let j of[...n]){f+=j=="("?1:-1;i++;if(f==-1)return i}}
 
 //safe
-//module.exports=n=>{let f=0,i=0;for(let j of[...n]){f+={"(":1,")":-1}[j]||0;i++;if(f==-1)return i}}
+//module.exports=n=>{let f=0,i=0;for(let j of[...n]){f+={"(":1,")":-1}[j]|0;i++;if(f==-1)return i}}
