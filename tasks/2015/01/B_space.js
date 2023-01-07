efdcec2a0d7edb38e2856b6b032b7eb9f6a5a634
rainforce15 <rainforce15@gmail.com>
2023-01-07 11:50:41 +0100

    reduced `let` usage in 2015/1 and 4, saving 1-2 bytes

--- tasks/2015/01/B_space.js
+++ tasks/2015/01/B_space.js
@@ -1,5 +1,5 @@
 //unsafe
-module.exports=n=>{let f=0,i=0;for(let j of[...n]){f+=j=="("?1:-1;i++;if(f==-1)return i}}
+module.exports=n=>{let f=0,i=0,j;for(j of[...n]){f+=j=="("?1:-1;i++;if(f==-1)return i}}
 
 //safe
-//module.exports=n=>{let f=0,i=0;for(let j of[...n]){f+={"(":1,")":-1}[j]|0;i++;if(f==-1)return i}}
+//module.exports=n=>{let f=0,i=0,j;for(j of[...n]){f+={"(":1,")":-1}[j]|0;i++;if(f==-1)return i}}
--- tasks/2015/04/A_space.js
+++ tasks/2015/04/A_space.js
@@ -1,1 +1,1 @@
-let c=require("crypto"),i;module.exports=n=>{for(i=0;;i++)if(c.createHash("md5").update(n+i).digest("hex").substring(0,5)=="00000")return i}
+let c=require("crypto"),i=0;module.exports=n=>{for(;;i++)if(c.createHash("md5").update(n+i).digest("hex").substring(0,5)=="00000")return i}
